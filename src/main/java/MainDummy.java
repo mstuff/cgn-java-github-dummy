ca0a69cc041327331b0723903cdbc09b990d2325
mstuff <mstuff@posteo.de>
2022-04-06 19:38:28 +0200

    Add sout in Main

---
+++ src/main/java/MainDummy.java
@@ -0,0 +1,6 @@
+public class MainDummy {
+    public static void main(String[] args) {
+        System.out.println("Hier ist der Dummy :) \n" +
+                "habe das Template von Stefan genommen");
+    }
+}
