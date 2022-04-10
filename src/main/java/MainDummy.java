86ea9c1e70aa7f38d4ad96ae816e3a3de19861da
wodie99 <wodie99@wodie.net>
2022-04-10 14:39:06 +0200

    Hotfix-Message in main added

--- src/main/java/MainDummy.java
+++ src/main/java/MainDummy.java
@@ -2,5 +2,6 @@ public class MainDummy {
     public static void main(String[] args) {
         System.out.println("Hier ist der Dummy :) \n" +
                 "habe das Template von Stefan genommen");
+        System.out.println("Hotfix eingebaut - alles wieder OK!");
     }
 }
