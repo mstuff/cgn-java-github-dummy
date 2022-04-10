80e81f996f0c9a2ff8ccca7496de77e0235b7fe4
wodie99 <wodie99@wodie.net>
2022-04-10 13:50:42 +0200

    first commit in main. Start of new class Helper - not finished!

---
+++ src/main/java/Helper.java
@@ -0,0 +1,6 @@
+public class Helper {
+
+    public Helper() {
+        System.out.println("Arbeit an Class Helper begonnen - noch nicht fertig!!!");
+    }
+}
--- src/main/java/MainDummy.java
+++ src/main/java/MainDummy.java
@@ -2,5 +2,7 @@ public class MainDummy {
     public static void main(String[] args) {
         System.out.println("Hier ist der Dummy :) \n" +
                 "habe das Template von Stefan genommen");
+
+        Helper helper = new Helper();
     }
 }
